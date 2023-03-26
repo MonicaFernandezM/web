d5809049e17e142b1cb650982ac567a85bbf5c1f
monicamfm <monicamfmiras@gmail.com>
2023-03-26 18:13:25 +0200

    Presentacion de Jinja

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/") #punto de salida lo que hay dentro del parentesis 
 def la_funcion():
-    return "Hola, mundo!"
+    return render_template("mipagina.html")
 
 
-@app.route("/bye/<nombre>")
-def otra_funcion(nombre):
-    return f"hasta luego {nombre}"
+@app.route("/bye/<nombre>/<apellido>")
+def otra_funcion(nombre, apellido):
+    return render_template("paginadespedida.html", name=nombre, surname=apellido)
+    return f"hasta luego {nombre} {apellido}"
+
